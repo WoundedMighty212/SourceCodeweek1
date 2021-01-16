0f0dfc48cb66f887c2093e9a4e295c04fae8051f
WoundedMighty212 <60252097+WoundedMighty212@users.noreply.github.com>
2021-01-16 13:47:49 +0200

    Add files via upload

---
+++ normalUser.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class normalUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.txtquantity = new System.Windows.Forms.TextBox();
+            this.cmbDescription = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnclear = new System.Windows.Forms.Button();
+            this.btnClearQuantity = new System.Windows.Forms.Button();
+            this.btnClearSearch = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lbSearch = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Description";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Quantity";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "item Code";
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(28, 47);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(159, 20);
+            this.txtCode.TabIndex = 3;
+            // 
+            // txtquantity
+            // 
+            this.txtquantity.Location = new System.Drawing.Point(22, 225);
+            this.txtquantity.Name = "txtquantity";
+            this.txtquantity.Size = new System.Drawing.Size(159, 20);
+            this.txtquantity.TabIndex = 4;
+            // 
+            // cmbDescription
+            // 
+            this.cmbDescription.FormattingEnabled = true;
+            this.cmbDescription.Location = new System.Drawing.Point(22, 136);
+            this.cmbDescription.Name = "cmbDescription";
+            this.cmbDescription.Size = new System.Drawing.Size(159, 21);
+            this.cmbDescription.TabIndex = 5;
+            this.cmbDescription.SelectedIndexChanged += new System.EventHandler(this.cmbDescription_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(614, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Search";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(617, 46);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(171, 20);
+            this.txtSearch.TabIndex = 7;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(673, 314);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 8;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnclear
+            // 
+            this.btnclear.Location = new System.Drawing.Point(580, 314);
+            this.btnclear.Name = "btnclear";
+            this.btnclear.Size = new System.Drawing.Size(75, 23);
+            this.btnclear.TabIndex = 9;
+            this.btnclear.Text = "Clear All";
+            this.btnclear.UseVisualStyleBackColor = true;
+            this.btnclear.Click += new System.EventHandler(this.btnclear_Click);
+            // 
+            // btnClearQuantity
+            // 
+            this.btnClearQuantity.Location = new System.Drawing.Point(208, 222);
+            this.btnClearQuantity.Name = "btnClearQuantity";
+            this.btnClearQuantity.Size = new System.Drawing.Size(75, 23);
+            this.btnClearQuantity.TabIndex = 10;
+            this.btnClearQuantity.Text = "clear";
+            this.btnClearQuantity.UseVisualStyleBackColor = true;
+            this.btnClearQuantity.Click += new System.EventHandler(this.btnClearQuantity_Click);
+            // 
+            // btnClearSearch
+            // 
+            this.btnClearSearch.Location = new System.Drawing.Point(530, 43);
+            this.btnClearSearch.Name = "btnClearSearch";
+            this.btnClearSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnClearSearch.TabIndex = 11;
+            this.btnClearSearch.Text = "Clear";
+            this.btnClearSearch.UseVisualStyleBackColor = true;
+            this.btnClearSearch.Click += new System.EventHandler(this.btnClearSearch_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(530, 83);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 12;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lbSearch
+            // 
+            this.lbSearch.FormattingEnabled = true;
+            this.lbSearch.Location = new System.Drawing.Point(334, 21);
+            this.lbSearch.Name = "lbSearch";
+            this.lbSearch.Size = new System.Drawing.Size(120, 95);
+            this.lbSearch.TabIndex = 14;
+            this.lbSearch.SelectedIndexChanged += new System.EventHandler(this.lbSearch_SelectedIndexChanged);
+            // 
+            // normalUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbSearch);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnClearSearch);
+            this.Controls.Add(this.btnClearQuantity);
+            this.Controls.Add(this.btnclear);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmbDescription);
+            this.Controls.Add(this.txtquantity);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "normalUser";
+            this.Text = "normalUser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.TextBox txtquantity;
+        private System.Windows.Forms.ComboBox cmbDescription;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnclear;
+        private System.Windows.Forms.Button btnClearQuantity;
+        private System.Windows.Forms.Button btnClearSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ListBox lbSearch;
+    }
+}
---
+++ normalUser.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using DataAccessLayer;
+namespace WindowsFormsApp1
+{
+    public partial class normalUser : Form
+    {
+        private List<Stocks> list;
+        public normalUser()
+        {
+            InitializeComponent();
+            list = new DataAccess().ReadDataBase();
+        }
+
+        private void btnclear_Click(object sender, EventArgs e)
+        {
+            txtCode.Text = string.Empty;
+            txtquantity.Text = string.Empty;
+            txtSearch.Text = string.Empty;
+            //combobox needs to go here maybe
+        }
+
+        private void btnClearSearch_Click(object sender, EventArgs e)
+        {
+            txtSearch.Text = string.Empty;
+        }
+
+        private void btnClearQuantity_Click(object sender, EventArgs e)
+        {
+            txtquantity.Text = string.Empty;
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string itemID = txtCode.Text;
+            string quantity = txtquantity.Text;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string searchText = txtSearch.Text;
+            List<string> searchResults = new List<string>();
+            foreach (var element in list)
+            {
+                if(searchText == element.ItemCode)
+                {
+                    searchResults.Add(element.ItemCode);
+                }
+                else if (searchText == element.Description)
+                {
+                    searchResults.Add(element.Description);
+                }
+            }
+            lbSearch.DataSource = searchResults;
+        }
+        private void lbSearch_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           string search = lbSearch.SelectedItem.ToString();
+           foreach(var element in list)
+            {
+                if (element.ItemCode == search)
+                {
+                    txtCode.Text = element.ItemCode;
+                    cmbDescription.SelectedItem = element.Description;
+                }
+                else if(element.Description == search)
+                {
+                    txtCode.Text = element.ItemCode;
+                    cmbDescription.SelectedItem = element.Description;
+                }
+            }
+        }
+        private void cmbDescription_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            List<string> desList = new List<string>();
+            while(list.GetEnumerator().MoveNext())
+            desList.Add(list.GetEnumerator().Current.Description);
+            cmbDescription.DataSource = desList;
+        }
+    }
+}
